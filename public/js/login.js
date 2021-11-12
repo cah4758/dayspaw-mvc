3a516eba8080f8280e43f20b0e6c9276b9e60d7d
NuminousBlue <jdiehl2236@gmail.com>
2021-11-11 22:01:00 -0600

    static js to handle login

--- public/js/login.js
+++ public/js/login.js
@@ -1,12 +1,11 @@
 const loginHandler = async (event) => {
   event.preventDefault();
-
-  // Collect values from the login form
+     // Collect input field values
   const name = document.querySelector('#user').value.trim();
   const password = document.querySelector('#password').value.trim();
 
   if (name && password) {
-    // Send a POST request to the API endpoint
+    // Send a POST request to the API endpoint to grab and deconstruct the body object 
     const response = await fetch('/users', {
       method: 'POST',
       body: JSON.stringify({ name, password }),
