2b00c0d139645224a1168371534749b1c3ebccbd
HubenakShane <87452894+HubenakShane@users.noreply.github.com>
2021-11-13 13:28:24 -0600

    Add files via upload
    SQL start to a table & chicken scratch -- clock starts on line 44

---
+++ Sql.js
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.sql*;
+
+
+public static void main( String [] args )throws Exception {
+
+String dogs[] = {
+    "Fluffiles",
+    "Clifford",
+    "Fido",
+    "Moto",
+    "Patches",
+    "spot"
+};
+
